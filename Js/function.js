8e28f6894c6eba750cf60a6a232f817ea620a2d2
Gera-MAJ <jatipg@gmail.com>
2023-02-28 20:40:10 -0300

    agregando el js

---
+++ Js/function.js
@@ -0,0 +1,14 @@
+
+const btnTitulos = document.getElementById("btnTitulos").addEventListener("click", btnTitulo())
+const btnLaboral = document.getElementById("btnLaboral").addEventListener("click", btnLaboral())
+
+const Titulos = document.getElementById("titulos")
+const Laboral = document.getElementById("laboral")
+
+function btnTitulo() {
+    Titulos.style.tog
+}
+
+function btnLaboral() {
+
+}
