014c05a39b0f892a983d5653b9c5eb5e1c597ae4
Gera-MAJ <jatipg@gmail.com>
2023-02-28 22:29:39 -0300

    poniendo los IF para que aparezca y desaparezcan

--- Js/function.js
+++ Js/function.js
@@ -1,14 +1,33 @@
 
-const btnTitulos = document.getElementById("btnTitulos").addEventListener("click", btnTitulo())
-const btnLaboral = document.getElementById("btnLaboral").addEventListener("click", btnLaboral())
 
-const Titulos = document.getElementById("titulos")
-const Laboral = document.getElementById("laboral")
+    document.getElementById("btnTitulos").addEventListener("click", btnTitulo)
+    document.getElementById("btnLaboral").addEventListener("click", btnLaboral)
 
-function btnTitulo() {
-    Titulos.style.tog
-}
+    const titulo = document.getElementById("titulos")
+    const laboral = document.getElementById("laboral")
 
-function btnLaboral() {
+    function btnTitulo() {
+        // acá se hace un IF común, clásico
+        
+        if (titulo.style.display == 'none') {
+            titulo.style.display = 'block'
+        }else{
+            titulo.style.display = 'none'
+        }
+    }
 
-}
+
+
+    function btnLaboral() {
+        // este es un IF es resumido, con menos lineas de código, pero es exactamente lo mismo
+
+        laboral.style.display = (laboral.style.display == 'none') ? 'block' : 'none';
+    }
+
+
+
+   
+
+
+
+
